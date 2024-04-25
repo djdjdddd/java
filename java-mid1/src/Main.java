30bf2b85ff30e2b1a31e1fc202c5662068e85739
djdjdddd <djdj2297@gmail.com>
2024-04-26 01:41:01 +0900

    김영한의 실전 자바 - 중급 1편

---
+++ java-mid1/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author : yong
+ * @fileName : ${NAME}
+ * @date : 2024-04-26
+ * @description :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
