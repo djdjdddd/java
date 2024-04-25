c4c764a0e05b9cbd60a09c09d1e9e723455bf64d
djdjdddd <djdj2297@gmail.com>
2024-04-26 01:40:16 +0900

    Feat: 섹션 3. String 클래스 - 기본

---
+++ java-mid1/src/lang/string/CharArrayMain.java
@@ -0,0 +1,19 @@
+package lang.string;
+
+/**
+ * @author : yong
+ * @fileName : CharArrayMain
+ * @date : 2024-04-26
+ * @description :
+ */
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+    }
+
+}
---
+++ java-mid1/src/lang/string/StringBasicMain.java
@@ -0,0 +1,19 @@
+package lang.string;
+
+/**
+ * @author : yong
+ * @fileName : StringBasicMain
+ * @date : 2024-04-26
+ * @description :
+ */
+public class StringBasicMain {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello");
+
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
+    }
+
+}
