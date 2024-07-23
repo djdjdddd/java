f2e37e590b5138adf4f82ec9e68ec0d4b478c7d9
djdjdddd <djdj2297@gmail.com>
2024-07-23 23:24:55 +0900

    Feat: 섹션 2. 제네릭 - Genenric2
    - 타입 매개변수 제한3~4

---
+++ java-mid2/src/generic/ex3/AnimalHospitalV2.java
@@ -0,0 +1,23 @@
+package generic.ex3;
+
+//public class AnimalHospitalV2<T> {
+//
+//	private T animal;
+//
+//	public void setAnimal(T animal) {
+//		this.animal = animal;
+//	}
+//
+//	public void checkup(){
+//		// 컴파일 오류
+//		System.out.println("동물 이름: " + animal.getName());
+//		System.out.println("동물 크기: " + animal.getSize());
+//		animal.sound();
+//	}
+//
+//	public T bigger(T target){
+//		// 컴파일 오류
+//		return animal.getSize() > target.getSize() ?  animal : target;
+//	}
+//
+//}
---
+++ java-mid2/src/generic/ex3/AnimalHospitalV3.java
@@ -0,0 +1,25 @@
+package generic.ex3;
+
+import generic.animal.Animal;
+
+public class AnimalHospitalV3<T extends Animal> {
+
+	private T animal;
+
+	public void setAnimal(T animal) {
+		this.animal = animal;
+	}
+
+	public void checkup(){
+		// 컴파일 오류
+		System.out.println("동물 이름: " + animal.getName());
+		System.out.println("동물 크기: " + animal.getSize());
+		animal.sound();
+	}
+
+	public T bigger(T target){
+		// 컴파일 오류
+		return animal.getSize() > target.getSize() ?  animal : target;
+	}
+
+}
